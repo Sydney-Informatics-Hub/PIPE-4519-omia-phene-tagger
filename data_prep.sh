4c80f3403fb6bfda42232be8111f3a1b32fdfb78
marius-mather <marius.mather@sydney.edu.au>
2023-11-13 14:46:15 +1100

    Update data prep script with build_distant_corpus

--- data_prep.sh
+++ data_prep.sh
@@ -1,2 +1,10 @@
 # Create a dictionary of terms from the ontology
 python omia_tagger/build_dict.py --input data/mp.obo --output data/mpo_dict/
+# Build the "distantly-supervised training dataset"
+#   Requires negative examples: see get_negative_examples.sh
+# TODO: build_distant_corpus is finicky about paths, e.g. --dict path must end with a /
+python omia_tagger/build_distant_corpus.py \
+  --dict data/mpo_dict/ \
+  --fileneg data/mutation_disease.txt \
+  --negnum 10000 \
+  --output data/distant_train_data/
