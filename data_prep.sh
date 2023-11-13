73209f36b8ec202613b7c63e98ad21334f39e1f2
marius-mather <marius.mather@sydney.edu.au>
2023-11-13 11:51:32 +1100

    Basic scripts for the initial stages of data download/prep

---
+++ data_prep.sh
@@ -0,0 +1,2 @@
+# Create a dictionary of terms from the ontology
+python omia_tagger/build_dict.py --input data/mp.obo --output data/mpo_dict/
---
+++ download_mpo.sh
@@ -0,0 +1,1 @@
+curl -L http://purl.obolibrary.org/obo/mp.obo --create-dirs -o data/mp.obo
