e2304a9921d8e2a6f01cde3d268ef7a64afecbad
marius-mather <marius.mather@sydney.edu.au>
2023-11-14 13:57:24 +1100

    Need to specify rootnode when building dictionary for MPO

--- data_prep.sh
+++ data_prep.sh
@@ -1,8 +1,12 @@
 # Create a dictionary of terms from the ontology
-python omia_tagger/build_dict.py --input data/mp.obo --output data/mpo_dict/
+python omia_tagger/build_dict.py \
+  --input data/mp.obo \
+  --output data/mpo_dict/ \
+  --rootnode MP:0000001
 # Build the "distantly-supervised training dataset"
 #   Requires negative examples: see get_negative_examples.sh
 # TODO: build_distant_corpus is finicky about paths, e.g. --dict path must end with a /
+#   Same with build_dict and output folder
 python omia_tagger/build_distant_corpus.py \
   --dict data/mpo_dict/ \
   --fileneg data/mutation_disease.txt \
